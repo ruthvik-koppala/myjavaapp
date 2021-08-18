94047c18c126c353704337abcd6df113a555744a
ruthvik-koppala <sri.sai.ruthvik.koppala@oracle.com>
2021-08-18 16:13:31 +0530

    added new method called add

--- myapp-august-2021/src/com/ofss/HelloWorld.java
+++ myapp-august-2021/src/com/ofss/HelloWorld.java
@@ -2,9 +2,14 @@ package com.ofss;
 
 public class HelloWorld {
 
+	public static int add(int a, int b) {
+		return a + b;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello github");
+		System.out.println(add(5, 6));
 	}
 
 }
